f3bf9c9bc939128f799ea30e9c8f57994fbff0ad
Gabarros <gabarros17@gmail.com>
2018-08-02 20:59:18 -0300

    Banco de Dados

---
+++ app/src/main/java/com/example/gabriel/lista_trabalho3/activity/OficinaDetalhe.java
@@ -0,0 +1,13 @@
+package com.example.gabriel.lista_trabalho3.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OficinaDetalhe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oficina_detalhe);
+    }
+}
